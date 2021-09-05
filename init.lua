37b10a3d4f5dee884411b88b6b862202deaca25d
Fog3211 <fog3211@163.com>
2021-09-06 00:27:38 +0800

    chore: split dir

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@
 -- LastEditTime: 2021-08-24 13:46:01
 -- Description: 一些自动化脚本
 --
-require 'switch-clashx-by-wifi'
+require 'modules.switch-clashx-by-wifi'
 -- require 'batch-git-pull'
-require 'reload-hs-config'
+require 'modules.reload-hs-config'
+require 'modules.auto-update'
---
+++ modules/auto-update.lua
@@ -0,0 +1,7 @@
+--[[
+Date: 2021-09-06 00:20:53
+LastEditTime: 2021-09-06 00:20:53
+Description: 自动更新各种语言及工具库
+--]]
+
+hs.alert.show('aaa')
